2457e6dd02f132639d5ec6087f345e6ab69d2972
Loksubhash Pulivarthi <Loksp27@tamu.edu>
2024-03-05 22:33:26 -0600

    Render Video screen with different mustaches

---
+++ elevate/ VideoRecordingView.swift
@@ -0,0 +1,20 @@
+//
+//   VideoRecordingView.swift
+//  elevate
+//
+//  Created by Loksubhash Pulivarthi on 3/5/24.
+//
+
+import SwiftUI
+
+struct VideoRecordingView: View {
+    var body: some View {
+        //
+        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VideoScreen()
+    }
+}
+
+#Preview {
+    VideoRecordingView()
+}
---
+++ elevate/RecordingListScreen.swift
@@ -0,0 +1,18 @@
+//
+//  RecordingListScreen.swift
+//  elevate
+//
+//  Created by Loksubhash Pulivarthi on 3/5/24.
+//
+
+import SwiftUI
+
+struct RecordingListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordingListScreen()
+}
